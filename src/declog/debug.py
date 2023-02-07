03ebee0449f6f26cd994f7db7c8497de97af7b69
Josh Read <joshua-read@hotmail.co.uk>
2023-02-07 21:44:47 +0000

    Polish StdOutLogger formatting

--- src/declog/debug.py
+++ src/declog/debug.py
@@ -4,24 +4,14 @@ from declog.logger import Logger, log
 class StdOutLogger(Logger):
 
     def __call__(self, *args, **kwargs):
-        arg_dict = self.build_arg_dict(args, kwargs)
-        print(arg_dict)
+        self.print_signature(args, kwargs)
         result = self._func(*args, **kwargs)
-        print(result)
-        return result
+        print(f"returns {result}")
 
     def log(self, key, value):
-        print(f'{key}: {value}')
+        print(f"{key} = {value}")
 
-
-if __name__ == '__main__':
-
-    @StdOutLogger
-    def my_function(a, b, c=3, d=-2):
-        ab = a * b
-        log('ab', ab)
-        cd = c / d
-        log(cd)
-        return ab + cd
-
-    my_function(3, 7)
+    def print_signature(self, args, kwargs):
+        arg_dict = self.build_arg_dict(args, kwargs)
+        arg_str = ", ".join(f"{k}={v}" for k, v in arg_dict.items())
+        print(f"{self._func.__name__}({arg_str})")
