809dec90e4634cbfb7d3fecf8ca99c52d6ffe992
Josh Read <joshua-read@hotmail.co.uk>
2023-02-07 20:25:41 +0000

    Update project to use dynamic versioning

--- src/declog/__init__.py
+++ src/declog/__init__.py
@@ -1,5 +1,13 @@
 from declog.debug import StdOutLogger
 from declog.logger import Logger, log
+from importlib.metadata import version, PackageNotFoundError
 
 
 __all__ = ["Logger", "log", "StdOutLogger"]
+
+try:
+    __version__ = version("declog")
+except PackageNotFoundError:
+    pass
+else:
+    __all__.append("__version__")
