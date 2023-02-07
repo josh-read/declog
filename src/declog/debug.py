d79cde4058fe441b2e46fdece1c4c99b4004bb8f
Josh Read <joshua-read@hotmail.co.uk>
2023-02-07 21:45:06 +0000

    Add StdOutLogger doctest

--- src/declog/debug.py
+++ src/declog/debug.py
@@ -2,6 +2,27 @@ from declog.logger import Logger, log
 
 
 class StdOutLogger(Logger):
+    """
+    Logger implementation which echos captured values to StdOut.
+
+    Examples:
+        The doctest below illustrates typical syntax and the types of values that can be captured, however the StdOutLogger
+        cannot demonstrate
+
+        >>> @StdOutLogger
+        ... def my_function(a, b, c=3, d=-2):
+        ...     ab = a * b
+        ...     log('ab', ab)
+        ...     cd = c / d
+        ...     log(cd)
+        ...     return ab + cd
+        ...
+        >>> my_function(3, 7, d=4)
+        my_function(a=3, b=7, c=3, d=4)
+        ab = 21
+        cd = 0.75
+        returns 21.75
+    """
 
     def __call__(self, *args, **kwargs):
         self.print_signature(args, kwargs)
