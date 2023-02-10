4504bd2158944673d644581502afe1bab2fdca95
Josh Read <joshua-read@hotmail.co.uk>
2023-02-10 20:23:51 +0000

    Update Logger to work with Database

--- src/declog/databases/database.py
+++ src/declog/databases/database.py
@@ -8,12 +8,40 @@ class Database(UserDict):
         return self[key]
 
 
-if __name__ == '__main__':
+def test_database_default_keys():
     db = Database()
-    keys = 'these are my keys'.split()
+    my_dict = {
+        'a': 'b',
+        'c': 'd',
+    }
 
     entry = db
-    for k in keys:
-        entry = entry[k]
+    for k in my_dict.keys():
+        value = my_dict[k]
+        entry = entry[value]
 
     print(db)
+
+
+def test_with_logger():
+    from declog.loggers.logger import Logger, log
+
+    class MyLogger(Logger):
+        db = Database()
+        unique_keys = 'function_name datetime'.split()
+
+    @MyLogger
+    def my_function(a, b, c=3, d=-2):
+        ab = a * b
+        log('ab', ab)
+        cd = c / d
+        log(cd)
+        return ab + cd
+
+    my_function(1, 2)
+    print(my_function.db)
+
+
+if __name__ == '__main__':
+    test_database_default_keys()
+    test_with_logger()
