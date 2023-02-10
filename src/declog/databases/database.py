96aa9c84d841282044c6670e8a5ae56fe0df609e
Josh Read <joshua-read@hotmail.co.uk>
2023-02-10 20:44:28 +0000

    Replace the StdOutLogger with a StdOutDatabase which just prints the database state every time it is updated.

--- src/declog/databases/database.py
+++ src/declog/databases/database.py
@@ -3,8 +3,16 @@ from collections import UserDict
 
 class Database(UserDict):
 
+    def __init__(self, root=None):
+        """root is not meant to be used by the user"""
+        if root is None:
+            self.root = self
+        else:
+            self.root = root
+        super().__init__()
+
     def __missing__(self, key):
-        self[key] = Database()
+        self[key] = self.__class__(root=self.root)
         return self[key]
 
 
