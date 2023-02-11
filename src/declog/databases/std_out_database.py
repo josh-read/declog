abc8bef8360bd212d2143325186c4767937240d5
Josh Read <joshua-read@hotmail.co.uk>
2023-02-11 21:36:35 +0000

    Move integration tests into separate directory

--- src/declog/databases/std_out_database.py
+++ src/declog/databases/std_out_database.py
@@ -10,26 +10,3 @@ class StdOutDatabase(Database):
     def __missing__(self, key):
         self[key] = StdOutDatabase(root=self.root)
         return self[key]
-
-
-def test_with_logger():
-    from declog.loggers.logger import Logger, log
-
-    class MyLogger(Logger):
-        db = StdOutDatabase()
-        unique_keys = 'function_name datetime'.split()
-
-    @MyLogger
-    def my_function(a, b, c=3, d=-2):
-        ab = a * b
-        log('ab', ab)
-        cd = c / d
-        log(cd)
-        return ab + cd
-
-    my_function(1, 2)
-    print(my_function.db)
-
-
-if __name__ == '__main__':
-    test_with_logger()
