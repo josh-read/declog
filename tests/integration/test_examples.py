c23fc6dff79007745325475c0596112f85e2c90f
Josh Read <53308348+josh-read@users.noreply.github.com>
2023-02-24 13:11:05 +0000

    Add function to discover examples

--- tests/integration/test_examples.py
+++ tests/integration/test_examples.py
@@ -2,7 +2,17 @@ from pathlib import Path
 import runpy
 
 
+def discover_examples():
+    """Get a generator of every python file in the examples directory."""
+    example_directory = Path('examples')
+    return example_directory.glob('**/*.py')
+
+
 def test_facility_example():
     path_to_example = Path('examples/facility_example/logger.py')
     assert path_to_example.exists()
     runpy.run_path(str(path_to_example), run_name='__main__')
+
+
+if __name__ == '__main__':
+    print(list(discover_examples()))
