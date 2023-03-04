8b626d40db99278cfcfea759c8e98dd5a864b814
Josh Read <joshua-read@hotmail.co.uk>
2023-03-04 22:37:32 +0000

    Add type hints for BaseLogger

--- src/declog/logger/base_logger.py
+++ src/declog/logger/base_logger.py
@@ -1,7 +1,9 @@
 import inspect
 from functools import update_wrapper
+from typing import Type, Any
 
 from ..core import logged_property
+from ..database import BaseDatabase
 
 
 class BaseLogger:
@@ -10,17 +12,17 @@ class BaseLogger:
     The class attributes `db` and `unique_keys` should be set by
     subclasses."""
 
-    db = None
+    db: Type[BaseDatabase] = None
     unique_keys: list[str] = None
 
-    def __init__(self, func, **kwargs):
+    def __init__(self, func: callable, **kwargs):
         self._func = func
         for key, value in kwargs.items():
             setattr(self, key, logged_property(lambda instance: value))
         update_wrapper(self, self._func)
         self.db_entry = None  # generated at call time
 
-    def generate_db_entry_and_remove_keys_from_info(self, info: dict):
+    def generate_db_entry_and_remove_keys_from_info(self, info: dict) -> dict:
         """Returns the dictionary corresponding to the specific call entry"""
         if self.unique_keys is None:
             raise NotImplementedError
@@ -45,10 +47,10 @@ class BaseLogger:
         self.db_entry = None
         return result
 
-    def log(self, key, value):
+    def log(self, key: str, value: Any):
         self.db_entry[key] = value
 
-    def build_arg_dict(self, args, kwargs):
+    def build_arg_dict(self, args: list[Any], kwargs: dict[str:Any]) -> dict:
         positional_args = {
             k: v for k, v in zip(inspect.signature(self._func).parameters, args)
         }
@@ -62,7 +64,7 @@ class BaseLogger:
         )  # update defaults with supplied keyword arguments
         return positional_args | keyword_args
 
-    def build_env_dict(self):
+    def build_env_dict(self) -> dict:
         env_dict = {}
         cls = type(self)
         for obj_name in dir(self):
