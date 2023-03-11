3ca489994c4cf652844532ad0e27974a59447419
Josh Read <joshua-read@hotmail.co.uk>
2023-03-11 22:24:15 +0000

    Pull out shared methods of persistent databases to superclass

--- src/declog/database/pickle_database.py
+++ src/declog/database/pickle_database.py
@@ -2,15 +2,7 @@ from declog.database.base_database import BaseDatabase
 import pickle
 
 
-class PickleDatabase(BaseDatabase):
-    """BaseDatabase which gets written to a pickle file.
-
-    Drawback of pickle is the whole thing must be read
-    from path so for a big database this will be slow.
-
-    Upside is native python objects can be stored so absolutely
-    anything can be logged, while for others it is more limited."""
-
+class PersistentDatabase(BaseDatabase):
     def __init__(self, path):
         self.path = path
         super().__init__()
@@ -21,6 +13,16 @@ class PickleDatabase(BaseDatabase):
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.write()
+
+
+class PickleDatabase(PersistentDatabase):
+    """BaseDatabase which gets written to a pickle file.
+
+    Drawback of pickle is the whole thing must be read
+    from path so for a big database this will be slow.
+
+    Upside is native python objects can be stored so absolutely
+    anything can be logged, while for others it is more limited."""
 
     def read(self):
         try:
