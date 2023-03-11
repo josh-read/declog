7ccdb1b2d4df02a44fd4bb621771bfbcb5ed4aae
Josh Read <joshua-read@hotmail.co.uk>
2023-03-11 22:26:44 +0000

    Export JSONDatabase

--- src/declog/database/__init__.py
+++ src/declog/database/__init__.py
@@ -1,3 +1,3 @@
 from .base_database import BaseDatabase  # noqa F401
-from .pickle_database import PickleDatabase  # noqa F401
+from .pickle_database import PickleDatabase, JSONDatabase  # noqa F401
 from .std_out_database import StdOutDatabase  # noqa F401
