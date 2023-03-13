90c1d2285b7def5aad952a1101a382ffdd12d471
Josh Read <joshua-read@hotmail.co.uk>
2023-03-13 21:17:14 +0000

    Merge to and from dict functions into cast function

--- src/declog/database/base_database.py
+++ src/declog/database/base_database.py
@@ -1,4 +1,5 @@
 from collections import UserDict
+from collections.abc import MutableMapping
 
 
 class BaseDatabase(UserDict):
@@ -28,31 +29,21 @@ class BaseDatabase(UserDict):
     def write(self):
         raise NotImplementedError
 
+    @staticmethod
+    def cast(mutable_mapping, new_type):
+        out = new_type()
+        for key, value in mutable_mapping.items():
+            if isinstance(value, MutableMapping):
+                out[key] = BaseDatabase.cast(value, new_type)
+            else:
+                out[key] = value
+        return out
+
     def to_dict(self):
-        return self._to_dict(self.data)
-
-    @staticmethod
-    def _to_dict(database):
-        out = {}
-        for key, value in database.items():
-            if isinstance(value, BaseDatabase):
-                out[key] = BaseDatabase._to_dict(value)
-            else:
-                out[key] = value
-        return out
-
-    @staticmethod
-    def _from_dict(dictionary):
-        out = BaseDatabase()
-        for key, value in dictionary.items():
-            if isinstance(value, dict):
-                out[key] = BaseDatabase._from_dict(value)
-            else:
-                out[key] = value
-        return out
+        return BaseDatabase.cast(self.data, dict)
 
     @classmethod
     def from_dict(cls, dictionary):
         instance = cls()
-        instance.data = BaseDatabase._from_dict(dictionary)
+        instance.data = BaseDatabase.cast(dictionary, BaseDatabase)
         return instance
