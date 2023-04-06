89f5337f5c46fe95043258484e41066c23b767f5
Josh Read <joshua-read@hotmail.co.uk>
2023-04-06 16:02:21 +0100

    Refactor test_logger.py

--- tests/unit/test_logger.py
+++ tests/unit/test_logger.py
@@ -1,52 +1,51 @@
+import pytest
+
+from declog.database import BaseDatabase
 from declog.logger import BaseLogger
 from declog.logger.mixins import FunctionNameMixin
-from declog.database import BaseDatabase
 
 
-def test_nested_logger():
+@pytest.fixture()
+def logger():
     class MyLogger(BaseLogger, FunctionNameMixin):
         db = BaseDatabase()
         unique_keys = ["function_name"]
 
-    @MyLogger
+    return MyLogger
+
+
+def test_nested_logger(logger):
+    @logger
     def my_function(x):
         return my_function_inner(x)
 
-    @MyLogger
+    @logger
     def my_function_inner(x):
         return x
 
     my_function(1)
 
-    assert MyLogger.db.data == {
+    assert logger.db.data == {
         "my_function": {"x": 1, "result": 1},
         "my_function_inner": {"x": 1, "result": 1},
     }
 
 
-def test_argument_capture():
-    class MyLogger(BaseLogger, FunctionNameMixin):
-        db = BaseDatabase()
-        unique_keys = ["function_name"]
-
-    @MyLogger
+def test_argument_capture(logger):
+    @logger
     def my_function(a, b, c=3, d=-1):
         return a * b * d - c
 
     my_function(1, 2, d=4)
 
-    assert MyLogger.db == {"my_function": {"a": 1, "b": 2, "c": 3, "d": 4, "result": 5}}
+    assert logger.db == {"my_function": {"a": 1, "b": 2, "c": 3, "d": 4, "result": 5}}
 
 
-def test_duplicate_entries():
+def test_duplicate_entries(logger):
     """Duplicate keys are overwritten - highlights the importance of choosing
     non-conflicting keys"""
 
-    class MyLogger(BaseLogger, FunctionNameMixin):
-        db = BaseDatabase()
-        unique_keys = ["function_name"]
-
-    @MyLogger
+    @logger
     def my_function(x):
         return x
 
@@ -54,7 +53,7 @@ def test_duplicate_entries():
     my_function(1)
     my_function(2)
 
-    assert MyLogger.db == {"my_function": {"x": 2, "result": 2}}
+    assert logger.db == {"my_function": {"x": 2, "result": 2}}
 
 
 def test_set_method():
